331192711a42d05da242e498ed3f8ece67582ca8
Brian Stovall <hoppenmaus@gmail.com>
2016-02-02 16:18:37 -0500

    completed win condition. commented and refactored code. moved center controls down.

--- simon.js
+++ simon.js
@@ -1,12 +1,15 @@
 
 document.addEventListener('DOMContentLoaded', function Simon(){
-			//handle to the whole game
+			//handles to the interface elements
 			var gameBody = document.getElementById('container');
 			var score = document.getElementById('score');
 			var lcd = document.getElementById('lcd');
 			var strictButton = document.getElementById('strictButton');
 			var restartButton = document.getElementById('restartButton');
 			var strictLabel = document.getElementById('strictLabel');
+
+			//solve this length pattern to win
+			var winNumber = 20;
 
 			//position the gameBody
 			gameBody.style.top = ((window.innerHeight - gameBody.offsetHeight +
@@ -35,11 +38,13 @@ document.addEventListener('DOMContentLoaded', function Simon(){
 			var turn = 0; //the current guess 'position' 
 			var isStrict = false; //whether or not the game is in 'strict' mode
 
+			//resets the game
 			restartButton.addEventListener('click', function() {
 				pattern = [];
 				playNextRound();
 			});
 
+			//toggle strict mode and style button to show state
 			strictButton.addEventListener('click', function() {
 				isStrict = !isStrict;
 				strictButton.style['background-color'] = 
@@ -48,6 +53,8 @@ document.addEventListener('DOMContentLoaded', function Simon(){
 					(isStrict) ? 'yellow' : 'black';
 			});
 
+			//used when the message bar pauses the action -
+			//at the beginning of a new game
 			score.addEventListener('click', function() {
 				score.style.top = '0';
 				score.style['pointer-events'] = 'none';
@@ -55,6 +62,7 @@ document.addEventListener('DOMContentLoaded', function Simon(){
 				window.setTimeout(playNextRound, defaultTimeOut); 
 			});
 			
+			//builds the functions of the 4 main buttons
 			function initButton(element, soundElement, number) {
 				element.addEventListener('click', function(){
 					soundElement.currentTime = 0;
@@ -65,8 +73,9 @@ document.addEventListener('DOMContentLoaded', function Simon(){
 				});
 			}
 
-			function greet(message) { //give an intro message and wait for user click to start
-				allowMouse(false); //user shouldn't push any buttons yet
+			//give a message and wait for user click to start
+			function greet(message) { 
+				allowMouse(false); 
 				gameBody.className = '';
 				score.style['pointer-events'] = 'auto';
 				score.style.top = '49vh';
@@ -75,37 +84,62 @@ document.addEventListener('DOMContentLoaded', function Simon(){
 
 			//tests the user's guess after each turn
 			function testGuess() {
-				if (guess == pattern[turn]) {
-					turn++;
-					if (turn == pattern.length) {
-						turn = 0;
-						window.setTimeout(playNextRound, defaultTimeOut );
-					}
-					//turn mouse back on ONLY when there is more to guess
-					else allowMouse(true); 
-		    }
+				//the player has guessed correctly
+				if (guess == pattern[turn]) { 
+						turn++;
+
+						//the player has completed this pattern
+						if (turn == pattern.length) {
+
+							//the player has won
+							if (pattern.length === winNumber) { 
+								turn = 0;
+								pattern = [];
+								window.setTimeout(greet.bind(null, 'VICTORY! Click to start again.'),
+										defaultTimeOut );
+							}
+
+							//the player hasn't won, build on the pattern
+							else {
+								turn = 0;
+								window.setTimeout(playNextRound, defaultTimeOut );
+							}
+
+						}
+						//the current pattern isn't complete yet
+						else allowMouse(true); 
+				}
+				//the player guessed incorrectly
 				else lose();
 			}
 
-			function allowMouse(bool) { //pass a bool to enable or disable clicks
-				var arr = document.getElementsByClassName('button');
+			//enables or disables user clicks
+			function allowMouse(bool) { 
+				var arr = document.getElementsByClassName('pushable');
 				for (var i = 0; i < arr.length; i++)
 					arr[i].style['pointer-events'] = (bool) ? 'auto' : 'none';
 			}
 			
+			//shakes the gameBody and starts either replays the pattern
+			//or starts over, depending on strict state
 			function lose() {
 				gameBody.className += ' animated shake';	
 				turn = 0;
+
+				//in strict mode
 				if (isStrict) {
 					pattern = [];
 					window.setTimeout(greet.bind(null, 'Click here to start over.'), defaultTimeOut );
 				}
-				//we are not in strict mode
+
+				//not in strict mode
 				else {
 					window.setTimeout(playSequence, defaultTimeOut * 2.5, defaultTimeOut, pattern);
 				}
 			}
 
+			//fires off a chain of setTimeouts to the buttons
+			//based on seq to play a pattern
 			function playSequence(timeOut, seq) {
 				gameBody.className = '';
 				allowMouse(false);
@@ -132,17 +166,21 @@ document.addEventListener('DOMContentLoaded', function Simon(){
 				}
 			}
 
+			//adds a random color to the pattern and plays it
+			//also updates the lcd
 			function playNextRound() {
 				pattern.push(Math.floor(Math.random()*4));
 				lcd.textContent = pattern.length;
 				playSequence(defaultTimeOut, pattern);
 			}
 
+			//helper function to change innerText
 			function changeText(elem, text){
 				if (elem.innerText) elem.innerText = text
 				else elem.textContent = text;
 			}
 			
+			//sets up and starts the game running
 			function begin() {
 				initButton(buttons['red'], document.getElementById('redSound'), 0);
 				initButton(buttons['blue'], document.getElementById('blueSound'), 1);
